ab19f5818ed25d27d5142bb9ee0d99304a0a3a73
Will Brown <williambrown@williams-air.lan>
2019-07-18 00:05:56 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You are consistent with your color palette, etc. for your websites and you're so nice and helpful.",
+  "You are super sweet and I liked that kazoo video you played to make mari laugh",
+  "you have great ideas !",
+  "You are such a sweet person with an amazing personality that lights up the room.",
+  "Attention to detail and ability to find mistakes.",
+  "Your kindness and how comfortable it is to talk and open up to you!",
+  "that I'm able to talk about k-pop with you!",
+  "how you are always so willing to help people",
+  "How funny you are and easy you are to talk to!",
+  "Your kindness and creativity.",
+  "Your kindness and eagerness to talk to people!",
+  "how sweet and kind you are. I loved making my projects with you because of your amazing dedication and attitude. Thanks for making class so fascinating  :)",
+  "You're very funny, and I really like that we can talk in the train and off the camp.",
+  "Your kindness. I honestly don’t know how I could have gotten on the Caltrain without you and your positivity throughout is absolutely admirable."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
